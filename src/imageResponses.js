5f792051efe6bdc4dd525fa86460ae58752cb8db
etc2119 <etc2119@rit.edu>
2019-09-05 17:39:19 -0400

    assignment complete

---
+++ src/imageResponses.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const img = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+
+const getImage = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  response.write(img);
+  response.end();
+};
+
+// Exports
+module.exports.getImage = getImage;
